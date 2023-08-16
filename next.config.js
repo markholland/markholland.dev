07a27430a93f903c8a70dfff3fed52503e53c617
Mark Holland <mark@markholland.dev>
2023-08-16 13:30:37 +0200

    Add apple-app-site-association file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [{ key: 'content-type', value: 'application/json' }],
+      },
+    ];
+  },
   i18n: {
     locales: ['en-GB'],
-    defaultLocale: 'en-GB'
-  }
-}
+    defaultLocale: 'en-GB',
+  },
+};
