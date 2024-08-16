d22bcce4c98ab58e863774e2bcfd60f0a79a5754
Mark Holland <mark@markholland.dev>
2024-08-16 13:30:32 +0200

    Add Vercel Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider defaultTheme="system">
       {/* eslint-disable-next-line react/jsx-props-no-spreading*/}
       <Component {...pageProps} />
+      <SpeedInsights />
     </ThemeProvider>
   );
 };
